7dc3cad5c8793015f56c95b5b0f15234e0798346
Ryan Johnson <rjguitar98@yahoo.com>
2023-11-28 22:02:47 -0500

    Initial Commit

---
+++ MovieRandomizer/MovieRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRandomizerApp.swift
+//  MovieRandomizer
+//
+//  Created by Ryan Johnson on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
